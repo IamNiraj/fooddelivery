f33b33f08802f49ebdccb8b61e949b42bccb460e
Niraj Pradhan <redragons94@gmail.com>
2014-09-07 12:58:11 +0545

    logout added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  
   def show
     @user = User.find(params[:id])
   end
@@ -11,6 +12,7 @@ class UsersController < ApplicationController
   	@user = User.new(user_params)    # Not the final implementation!
     if @user.save
       # Handle a successful save.
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
