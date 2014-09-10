38330520a0693d1f625ed2ac865f1205dbe5fa57
Niraj Pradhan <redragons94@gmail.com>
2014-09-11 00:03:54 +0545

    edit added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,10 +20,28 @@ class UsersController < ApplicationController
     end
   end
 
+
+    def edit
+      @user= User.find(params[:id])
+    end
+
+
+    def update
+      @user= User.find(params[:id])
+      if @user.update_attributes(user_params)
+        flash[:success] = "successful update"
+        redirect_to @user
+      else
+        render 'edit'
+      end
+    end
+
+
   private
   	def user_params
   		params.require(:user).permit(:name,:email,:password,:password_confirmation)
    	end
   	
+
   
 end
